d07abd57862975c7f4253469e64d025a8a254d48
m.shrief <m.shrief.dev@proton.me>
2026-08-10 22:47:32 +0300

    Added testing for JWT signing & verifing

--- src/utils/auth.spec.ts
+++ src/utils/auth.spec.ts
@@ -1,7 +1,11 @@
 import { describe, expect, it, vi, test, beforeAll } from 'vitest';
 import bcrypt from "bcrypt"
+import {sign, verify} from 'hono/jwt'
+import { JWTPayload } from 'hono/utils/jwt/types';
 ////
-import {compare_password, hash_password} from "./auth.js"
+import {JWT_PRIVATE_KEY, JWT_PUBLIC_KEY} from '../config.js'
+import { RoleEnum } from '../database/schemas.js';
+import {OP, RoleEnumType, compare_password, create_permission, create_permissions, hash_password, sign_token, verify_token} from "./auth.js"
 
 describe.concurrent("", async() => {
     describe("for Password Hashing", async() => {
@@ -29,6 +33,81 @@ describe.concurrent("", async() => {
             let hash3 = await bcrypt.hash("other password", bcrypt.genSaltSync())
             is_valid = await compare_password(password, hash3)
             expect(is_valid).toBe(false)
+
+        })
+    })
+    describe("for JWT", async() => {
+        test("Testing sign_token()", async () => {
+            let user1 = {
+                id: "932ba7ba-8a4f-4282-8ca4-d0f9c0813bc1",
+                username: "username1",
+                roles: [RoleEnum.NORMAL, RoleEnum.DBA]
+            }
+            let exp_delta1 = 2
+            let token1 = await sign_token(user1.id, user1.username, user1.roles, exp_delta1) 
+            // if verify works without errors, then exp & iat are correct
+            let payload1 = await verify(token1, JWT_PUBLIC_KEY, "RS256") 
+            // To have the difference between iat & exp == exp_delta
+            expect((payload1.exp as number) - (payload1.iat as number)).toEqual(60 * 60 * exp_delta1)
+
+            let payload_user1 = payload1["user"] as typeof user1
+            expect(payload_user1["id"]).toEqual(user1.id)
+            expect(payload_user1["username"]).toEqual(user1.username)
+            expect(payload_user1["roles"]).toEqual(user1.roles)      
+
+            let payload_perms1 = payload1["permissions"] as string[]
+            expect(payload_perms1.length).toBe(user1.roles.length * 2)
+            expect(payload_perms1.indexOf(create_permission(user1.roles[0], OP.READ))).not.toEqual(-1)
+        })
+
+        test("Testing verify_token()", async() => {
+            let payload: JWTPayload | null
+            let now = Math.floor(Date.now() / 1000)
+            let roles1: RoleEnumType[] = [RoleEnum.NORMAL, RoleEnum.DBA]
+            let permissions1 = create_permissions(roles1)
+            let user1 = {
+                id: "932ba7ba-8a4f-4282-8ca4-d0f9c0813bc1",
+                username: "username1",
+                roles: roles1
+            }
+            let token1 = await sign(
+                {user: user1, permissions: permissions1, exp: now + 60 * 60 * 1,iat: now},
+                JWT_PRIVATE_KEY,
+                'RS256',
+            );
+            let auth_header1 = `Bearer ${token1}`
+            payload = await verify_token(auth_header1)
+            expect(payload).not.toBeNull()
+
+            if(payload) {
+                let payload_user1 = payload["user"] as typeof user1
+                expect(payload_user1["id"]).toEqual(user1.id)
+                expect(payload_user1["username"]).toEqual(user1.username)
+                expect(payload_user1["roles"]).toEqual(user1.roles)      
+
+                let payload_perms1 = payload["permissions"] as string[]
+                expect(payload_perms1.length).toBe(user1.roles.length * 2)
+                expect(payload_perms1.indexOf(create_permission(user1.roles[0], OP.READ))).not.toEqual(-1)
+            }
+
+            let token2 = await sign(
+                {user: user1, permissions: permissions1, exp: now + 60 * 60 * -1,iat: now},
+                JWT_PRIVATE_KEY,
+                'RS256',
+            );
+            let auth_header2 = `Bearer ${token2}`
+            payload = await verify_token(auth_header2)
+            expect(payload).toBeNull()
+
+
+            let token3 = await sign(
+                {user: user1, permissions: permissions1, exp: now + 60 * 60 * 2,iat: now + 60 * 60 * 1},
+                JWT_PRIVATE_KEY,
+                'RS256',
+            );
+            let auth_header3 = `Bearer ${token3}`
+            payload = await verify_token(auth_header3)
+            expect(payload).toBeNull()
 
         })
     })
