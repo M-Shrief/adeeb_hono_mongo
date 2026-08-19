27f804c2f23b13e3fa0b48ded865ebb993165e72
m.shrief <m.shrief.dev@proton.me>
2026-08-19 19:33:53 +0300

    Removed drizzle packagaes, and installed mongoose to migrate this instance to use MongoDB.

--- drizzle/0001_lowly_infant_terrible.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "users" ALTER COLUMN "roles" DROP DEFAULT;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "roles" SET DATA TYPE "public"."roles_enum"[] USING "roles"::text::"public"."roles_enum"[];--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "roles" SET DEFAULT '{"Normal"}';
--- drizzle/0002_round_ozymandias.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "adeebs" ALTER COLUMN "name" SET DATA TYPE varchar(128);--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "username" SET DATA TYPE varchar(128);
