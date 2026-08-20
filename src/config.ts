2c0796c1906a8335837cd6c65cc48ffdafec76b8
m.shrief <m.shrief.dev@proton.me>
2026-08-20 19:31:57 +0300

    Updated database and config for MongoDB instead of Postgres

--- src/config.ts
+++ src/config.ts
@@ -12,10 +12,8 @@ export const {
   DB_USER,
   DB_PASSWORD,
   DB_NAME,
-  DB_MIGRATION_FOLDER,
 } = process.env
 
-export const DB_URL = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
 
 export const NODE_ENV = process.env.NODE_ENV as "dev" | "prod"
 
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,20 +1,51 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import { MigrationConfig } from 'drizzle-orm/migrator';
-/////
-import {DB_URL, DB_MIGRATION_FOLDER, NODE_ENV} from "../config.js"
-import * as schemas from "./schemas.js"
+import mongoose from 'mongoose';
+// config
+import { NODE_ENV,  DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } from '../config.js';
+// Utils
+import { logger } from '../utils/logger.js';
 
+mongoose.set('strictQuery', true);
 
-export const db = drizzle({ 
-  schema: schemas,
-  connection: { 
-    url: DB_URL, 
-    ssl: false 
-  },
-  casing: "snake_case",
-  // logger: NODE_ENV == "dev" ? true : false 
+// Create the database connection
+const options = {
+  autoIndex: NODE_ENV == "dev" ? true : false,
+  minPoolSize: 5, // Maintain up to x socket connections
+  maxPoolSize: 10, // Maintain up to x socket connections
+  connectTimeoutMS: 10 * 1000, // Give up initial connection after 10 seconds
+  socketTimeoutMS: 45 * 1000, // Close sockets after 45 seconds of inactivity
+};
+
+const DB_URL = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+export const connectDB = async () => await mongoose.connect(DB_URL, options);
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', () => {
+  logger.info('Mongoose default connection open to: ' + DB_URL);
 });
 
-export const migration_config: MigrationConfig = {
-  migrationsFolder: DB_MIGRATION_FOLDER || './drizzle'
-}
+// If the connection throws an error
+mongoose.connection.on('error', (err) => {
+  logger.error(`can't connect to: ${DB_URL}`);
+  logger.error('error: ' + err);
+  // exit(1) to have PM2 start it again
+  process.exit(1);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', () => {
+  logger.info('Mongoose default connection disconnected');
+});
+
+// If you want to close the connect before terminating the app, use this:
+// 
+// process.on('SIGINT', async () => {
+//   await mongoose.connection.close(true);
+//   logger.info(
+//     'Mongoose default connection disconnected through app termination',
+//   );
+
+//   process.exit(0);
+// });
+
+export const connection = mongoose.connection;
