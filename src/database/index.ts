ac1310be20a4f827ad7d275327794099b487bab9
m.shrief <m.shrief.dev@proton.me>
2026-08-22 18:30:27 +0300

    Added the authSource for DB_URL, and modified connect option for autoCreate depending on the NODE_ENV, and used ConnectOptions type

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose, { ConnectOptions } from 'mongoose';
 // config
 import { NODE_ENV,  DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } from '../config.js';
 // Utils
@@ -7,16 +7,18 @@ import { logger } from '../utils/logger.js';
 mongoose.set('strictQuery', true);
 
 // Create the database connection
-const options = {
+const options: ConnectOptions = {
   autoIndex: NODE_ENV == "dev" ? true : false,
+  autoCreate: NODE_ENV == "dev" ? true : false,
   minPoolSize: 5, // Maintain up to x socket connections
   maxPoolSize: 10, // Maintain up to x socket connections
   connectTimeoutMS: 10 * 1000, // Give up initial connection after 10 seconds
-  socketTimeoutMS: 45 * 1000, // Close sockets after 45 seconds of inactivity
+  // socketTimeoutMS: 45 * 1000, // Close sockets after 45 seconds of inactivity
 };
 
-const DB_URL = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
-export const connectDB = async () => await mongoose.connect(DB_URL, options);
+const DB_URL = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+// we use top-level await, here
+export const conn =  await mongoose.connect(DB_URL, options);
 
 // CONNECTION EVENTS
 // When successfully connected
