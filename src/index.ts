12b5f68a9d62bba3deba718d129135fa0fd6b701
m.shrief <m.shrief.dev@proton.me>
2026-08-23 23:56:17 +0300

    Removed old imports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import { serve } from '@hono/node-server'
-import { migrate } from 'drizzle-orm/postgres-js/migrator';
 //////
 import {app} from './app.js';
-import { db, migration_config } from './database/index.js'
 import { on_process_failure } from './utils/errors.js'
 import { logger } from './utils/logger.js';
 
@@ -10,10 +8,7 @@ const start = async () => {
   try {
 
     on_process_failure()
-
-    // Database migration if it wasn't already migrated.
-    await migrate(db, migration_config)
-
+    
     serve({
       fetch: app.fetch,
       port: 3000,
